eb201f7275bcb001241d5465aa967c5beb60fd03
Mateusz Winiarczyk <matthew.winiarczyk@gmail.com>
2025-05-13 16:44:26 +0200

    feat: add collection_list view

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
+    path('collections/', views.collection_list, name='collection_list'),
     path('skins/', views.skin_list, name='skin_list'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from .models import Skin
+from .models import Collection, Skin
+
+
+def collection_list(request):
+    context = {
+        'collections': Collection.objects.all(),
+    }
+    return render(request, 'catalog/collection_list.html', context)
 
 
 def skin_list(request):

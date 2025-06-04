9a0d322bdcae4b795cccd45a788fa1afad2bff7c
Mateusz Winiarczyk <matthew.winiarczyk@gmail.com>
2025-06-04 16:50:49 +0200

    feat: separate weapon and armor collections in collection_list view

--- catalog/views.py
+++ catalog/views.py
@@ -3,8 +3,12 @@ from .models import Collection, Skin
 
 
 def collection_list(request):
+    weapon = Collection.objects.filter(category='Weapon').order_by('name')
+    armor = Collection.objects.filter(category='Armor').order_by('name')
+
     context = {
-        'collections': Collection.objects.all(),
+        'weapon_collections': weapon,
+        'armor_collections': armor,
     }
     return render(request, 'catalog/collection_list.html', context)
 

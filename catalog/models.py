162d47e1c15d051f3a1fc8401b20a47e75973df2
Mateusz Winiarczyk <matthew.winiarczyk@gmail.com>
2025-08-06 16:48:52 +0200

    style: reformat catalog models.py

--- catalog/models.py
+++ catalog/models.py
@@ -39,26 +39,42 @@ class Map(models.Model):
 
 
 class Collection(models.Model):
-    name = models.CharField(max_length=255)
-    category = models.CharField(max_length=20)
-    note = models.CharField(max_length=255)
+    name = models.CharField(
+        max_length=255,
+    )
+    category = models.CharField(
+        max_length=20,
+    )
+    note = models.CharField(
+        max_length=255,
+    )
 
     def __str__(self):
         return self.name
 
 
 class Skin(models.Model):
-    api_id = models.PositiveIntegerField(primary_key=True)
+    api_id = models.PositiveIntegerField(
+        primary_key=True,
+    )
+    name = models.CharField(
+        max_length=255,
+    )
+    category = models.CharField(
+        max_length=20,
+    )
+    details = models.JSONField(
+        default=dict,
+    )
+    is_unlocked = models.BooleanField(
+        default=False,
+    )
     collection = models.ForeignKey(
         Collection,
         on_delete=models.SET_NULL,
         null=True,
         related_name='skins',
     )
-    name = models.CharField(max_length=255)
-    category = models.CharField(max_length=20)
-    details = models.JSONField(default=dict)
-    is_unlocked = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name

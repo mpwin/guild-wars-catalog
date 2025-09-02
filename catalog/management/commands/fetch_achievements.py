db7ac299cfe2d64ef0e17036f5167b27852e47ae
Mateusz Winiarczyk <matthew.winiarczyk@gmail.com>
2025-09-02 17:50:24 +0200

    ops: add fetch_achievements management command

---
+++ catalog/management/commands/fetch_achievements.py
@@ -0,0 +1,28 @@
+from catalog.models import Achievement
+from catalog.services.gw2_api_client import GW2APIClient
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        def valid_achievement(data):
+            return bool(data.get('id')) and bool(data.get('name'))
+
+        api_client = GW2APIClient()
+
+        for data in filter(valid_achievement, api_client.iter_achievements()):
+            defaults = {
+                'name': data.get('name'),
+                'description': data.get('description'),
+            }
+
+            achievement, _ = Achievement.objects.update_or_create(
+                api_id=data['id'],
+                defaults=defaults,
+            )
+
+            self.stdout.write(
+                f"Achievement {achievement.api_id}"
+            )

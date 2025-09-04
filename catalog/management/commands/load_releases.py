7e7e0ceedfbbf50bb34eccf055189b99b76d6faa
Mateusz Winiarczyk <matthew.winiarczyk@gmail.com>
2025-09-04 14:37:41 +0200

    ops: add achievement loading to load_releases management command

--- catalog/management/commands/load_releases.py
+++ catalog/management/commands/load_releases.py
@@ -1,6 +1,6 @@
 import os
 import yaml
-from catalog.models import Collection, Release, Skin, Zone
+from catalog.models import Achievement, Collection, Release, Skin, Zone
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
@@ -39,6 +39,7 @@ class Command(BaseCommand):
                 )
                 zones[zone.slug] = zone
                 self.stdout.write(f"Zone : {zone.name}")
+                self.load_achievements(zone, zone_data['achievement_ids'])
 
             for collection_data in data['collections']:
                 collection, _ = Collection.objects.update_or_create(
@@ -57,3 +58,11 @@ class Command(BaseCommand):
                     if skin.collection_id != collection.id:
                         skin.collection = collection
                         skin.save(update_fields=['collection'])
+
+    def load_achievements(self, zone: Zone, ach_ids: list[int]) -> None:
+        for ach_id in ach_ids:
+            ach = Achievement.objects.get(api_id=ach_id)
+            ach.release = zone.release
+            ach.zone = zone
+            ach.save()
+            print(f"Achievement : {zone} ~ {ach.name}")
